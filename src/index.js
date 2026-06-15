6cb72bc9854f44f6d070c22dead259e70f399008
vanessaTres <vanescolar19@gmail.com>
2026-06-15 16:00:53 -0300

    Configura express.Router no arquivo principal index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 
-git commit -m "Cria estrutura inicial do projeto"
+const express = require('express');
+
+
+const produtosRouter = require('./routes/produtos');
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.use('/produtos', produtosRouter);
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
